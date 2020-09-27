1e86281b54f48e5ea0a5ccde9aacb68cf34c34c4
Brooke McBain <mcbainbrooke@gmail.com>
2020-09-26 23:48:55 -0500

    -re formatted a method in the customer class to dispense a soda if user enters exact change

--- Customer.cs
+++ Customer.cs
@@ -8,112 +8,104 @@ namespace Soda_Machine
     {
         Wallet wallet = new Wallet();
         Backpack backpack = new Backpack();
+        SodaMachine sodaMachine = new SodaMachine();
+        List<Backpack> backpackList = new List<Backpack>();
+        List<double> coinsInMachine = new List<double>();
+        Coin quarter = new Quarters();
+        Coin dimes = new Dimes();
+        Coin nickel = new Nickels();
+        Coin penny = new Pennies();
+        Can orangeSoda = new OrangeSoda();
+        Can cola = new Cola();
+        Can rootBeer = new RootBeer(); 
 
         public Customer()
         {
-            ChooseCoins(); 
-        }
-        public void Intro()
-        {
-            Console.WriteLine("Soda Machine");
-            Console.WriteLine("1. Orange Soda - $0.06");
-            Console.WriteLine("2. Coca Cola - $0.35");
-            Console.WriteLine("3. Root Beer - $0.60");
-        }
-        public void ChooseSoda()
-        {
-            SodaMachine sodaMachineOptions = new SodaMachine();
-            List<Backpack> backpack = new List<Backpack>();
-            Wallet wallet = new Wallet();
-
-            string answer = Console.ReadLine();
-
-            for (int index = 0; index < 3; index++)
-                if (answer == "1")
-                {
-                    // add to backpack 
-                    Console.WriteLine("Please wait while we dispense your 'Orange Soda'");
-                    sodaMachineOptions.RemoveOrangeSoda();
-                    break;
-
-                }
-                else if (answer == "2")
-                {
-                    // add to backpack
-                    Console.WriteLine("Please wait while we dispense your 'Coca Cola'");
-                    sodaMachineOptions.RemoveCocaCola();
-                    break;
-                }
-                else if (answer == "3")
-                {
-                    //add to backpack
-                    Console.WriteLine("Please wait while we dispense your 'Root Beer'");
-                    sodaMachineOptions.RemoveRootBeer();
-                    break;
-
-                }
-                else
-                {
-                    Console.WriteLine("Please choose an option from the list above, type a number 1- 3");
-                }
+           
         }
 
-        public void CoinsInWallet()
-        {
-            Wallet wallet = new Wallet();
-
-            Console.WriteLine("Please choose which coins you would like to use: ");
-            Console.WriteLine("Your wallet: ");
-            wallet.CoinsInWallet();
-
-        }
         public void ChooseCoins()
         {
-            Wallet wallet = new Wallet();
-            SodaMachine coinsInSodaMachine = new SodaMachine();
+            User_Interface.CoinList();
 
-            Console.WriteLine("Please choose which coins you would like to insert");
-            Console.WriteLine("1)"); wallet.DisplayQuarters();
-            Console.WriteLine("2)"); wallet.DisplayDimes();
-            Console.WriteLine("3)"); wallet.DisplayNickels();
-            Console.WriteLine("4)"); wallet.DisplayPennies();
-            Console.WriteLine("5) Done with coin transaction");
-
-
-            Console.WriteLine("Would you like to insert another coin");
             string answer = Console.ReadLine();
             for (int input = 0; input < 6; input++)
             {
                 if (answer == "1")
                 {
+                    coinsInMachine.Add(quarter.ValueOfCoin); 
                     wallet.RemoveQuarter();
-                    coinsInSodaMachine.AddQuarter();
+                    sodaMachine.AddQuarter();
                     ChooseCoins();
                 }
                 else if (answer == "2")
                 {
+                    coinsInMachine.Add(dimes.ValueOfCoin); 
                     wallet.RemoveDimes();
-                    coinsInSodaMachine.AddDime();
+                    sodaMachine.AddDime();
                     ChooseCoins();
                 }
                 else if (answer == "3")
                 {
+                    coinsInMachine.Add(nickel.ValueOfCoin); 
                     wallet.RemoveNickels();
-                    coinsInSodaMachine.AddNickel();
+                    sodaMachine.AddNickel();
                     ChooseCoins();
                 }
                 else if (answer == "4")
                 {
+                    coinsInMachine.Add(penny.ValueOfCoin); 
                     wallet.RemovePennies();
-                    coinsInSodaMachine.AddPenny();
+                    sodaMachine.AddPenny();
                     ChooseCoins();
                 }
                 else if (answer == "5")
                 {
-                    // continue on   
+                    wallet.CoinsInWallet(); 
                 }
-
+                else
+                {
+                    break;
+                }
             }
         }
+        public void ChooseSoda()
+        {
 
+            User_Interface.SodaList();
+            string answer = Console.ReadLine();
+
+            for (int index = 0; index < 3; index++)
+            {
+                if (coinsInMachine.Equals(orangeSoda.Cost) & answer == "1")
+                {
+                    backpack.AddOrangeSoda();
+                    Console.WriteLine("Please wait while we dispense your 'Orange Soda'");
+                    sodaMachine.RemoveOrangeSoda();
+                    break;
+                }
+                else if (coinsInMachine.Equals(cola.Cost) & answer == "2")
+                {
+                    backpack.AddCocaCola();
+                    Console.WriteLine("Please wait while we dispense your 'Coca Cola'");
+                    sodaMachine.RemoveCocaCola();
+                    break;
+                }
+                else if (coinsInMachine.Equals(rootBeer.Cost) & answer == "3")
+                {
+                    backpack.AddRootBeer();
+                    Console.WriteLine("Please wait while we dispense your 'Root Beer'");
+                    sodaMachine.RemoveRootBeer();
+                    break;
+
+                }
+            }
+               
+                     
+               
+        }
+
+ 
+        
     }
+}
